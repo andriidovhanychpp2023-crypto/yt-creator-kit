83b509684bf92ae890a4db32b98fb5b8017ebd16
andriidovhanych <andrii.dovhanych.pp.2023@lpnu.ua>
2026-02-22 04:56:05 +0200

    Lab3: Complete automation and build process for yt-creator-kit

--- lab1.py
+++ lab1.py
@@ -1,2 +1,1 @@
 print("Happy New Year!")
-
---
+++ tests/test_e2e.py
@@ -0,0 +1,26 @@
+from playwright.sync_api import sync_playwright
+
+
+def test_add_brainrot_meme_scenario():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+
+        # Відкриваємо сайт
+        page.goto("https://www.google.com")
+
+        # Критичний шлях: пошук "хайпового" контенту
+        search_field = page.locator("textarea[name='q']")
+        search_field.fill("Skibidi Rizz")
+        page.keyboard.press("Enter")
+
+        # Чекаємо, поки з'являться результати (елемент #search)
+        page.wait_for_selector("#search", timeout=15000)
+
+        # ПЕРЕВІРКА: чи видимий блок з результатами
+        results = page.locator("#search")
+        assert results.is_visible()
+
+        page.wait_for_timeout(10000)
+
+        browser.close()
